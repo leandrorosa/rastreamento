986768727d47caf94a4d94d108a708568d537c7b
Leandro Rosa <leandro.rosa@neogrid.com>
2017-07-12 12:58:39 -0300

    Adicionando controllers e modelo

---
+++ src/main/java/com/lrosa/rastreamento/model/Contact.java
@@ -0,0 +1,41 @@
+package com.lrosa.rastreamento.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    public Contact() {
+
+    }
+
+    public Contact(final String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+}
